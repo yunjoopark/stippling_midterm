9debf4013daa8104ec7de5568e2293a99a01e4f2
yunjoopark <yunjoopark12@gmail.com>
2015-11-25 17:16:34 +0900

    commit report part

---
+++ report/FIGS/hedcut/svg/convert.bat
@@ -0,0 +1,6 @@
+inkscape -D -z --file=phoenix-5000-h1.svg --export-pdf=phoenix-5000-h1.pdf --export-latex
+inkscape -D -z --file=phoenix-5000-h2.svg --export-pdf=phoenix-5000-h2.pdf --export-latex
+inkscape -D -z --file=phoenix-5000-h3.svg --export-pdf=phoenix-5000-h3.pdf --export-latex
+
+inkscape -D -z --file=phoenix-10000-h.svg --export-pdf=phoenix-10000-h.pdf --export-latex
+inkscape -D -z --file=phoenix-3000-h.svg --export-pdf=phoenix-3000-h.pdf --export-latex
