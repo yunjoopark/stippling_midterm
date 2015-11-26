785d00237a9aa6d0c8f06e9a85506588b2289f8c
yunjoopark <yunjoopark12@gmail.com>
2015-11-26 20:51:38 +0900

    commit svg for pooh_grayscale

---
+++ report/FIGS/pooh_grayscale/convert.bat
@@ -0,0 +1,11 @@
+inkscape -D -z --file=phoenix-5000-v1.svg --export-pdf=phoenix-5000-v1.pdf --export-latex
+inkscape -D -z --file=phoenix-5000-v2.svg --export-pdf=phoenix-5000-v2.pdf --export-latex
+inkscape -D -z --file=phoenix-5000-v3.svg --export-pdf=phoenix-5000-v3.pdf --export-latex
+
+inkscape -D -z --file=einstein-100-v-c.svg --export-pdf=einstein-100-v-c.pdf --export-latex
+inkscape -D -z --file=einstein-5000-v-c.svg --export-pdf=einstein-5000-v-c.pdf --export-latex
+inkscape -D -z --file=einstein-250000-v-c.svg --export-pdf=einstein-250000-v-c.pdf --export-latex
+
+inkscape -D -z --file=einstein-medium-100-v-c.svg --export-pdf=einstein-medium-100-v-c.pdf --export-latex
+inkscape -D -z --file=einstein-medium-5000-v-c.svg --export-pdf=einstein-medium-5000-v-c.pdf --export-latex
+inkscape -D -z --file=einstein-medium-250000-v-c.svg --export-pdf=einstein-medium-250000-v-c.pdf --export-latex
